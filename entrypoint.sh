1317ce46014b8a6150715f72011347d4f7b8881a
Dirk Lüth <dirk.lueth@gmail.com>
2019-06-08 12:14:23 +0200

    fix creation of user and group

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,9 +29,17 @@ fi
 ln -fs /usr/share/zoneinfo/${TZ} /etc/localtime
 dpkg-reconfigure -f noninteractive tzdata
 
-groupadd --force --gid ${PGID} easyepg
-useradd --uid ${PUID} --gid ${PGID} --home /easyepg --shell /bin/bash easyepg
+if ! grep -q ${PGID} /etc/group; then
+  groupadd --gid ${PGID} easyepg
+fi
+
+if ! id -u easyepg &> /dev/null; then
+  useradd --uid ${PUID} --gid ${PGID} --home /easyepg --shell /bin/bash easyepg
+fi
+
 chown -R ${PUID}:${PGID} /easyepg
 chown -R ${PUID}:${PGID} /tmp
 
-su easyepg -c "cd /easyepg && ./epg.sh"
+USERNAME=$(getent passwd ${PUID} | cut -d: -f1)
+
+su ${USERNAME} -c "cd /easyepg && ./epg.sh"
